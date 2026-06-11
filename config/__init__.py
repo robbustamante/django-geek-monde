2bc669342827e996026da5285b10299dc276e037
robbustamante <robertobustamanteg5@gmail.com>
2026-06-10 23:38:50 -0300

    feat: implement product serializers, Celery configuration, and notification email template

--- config/__init__.py
+++ config/__init__.py
@@ -2,3 +2,8 @@
 Django Geek Monde - E-commerce Platform for Geek Clothing and Articles
 """
 __version__ = '1.0.0'
+
+# Make Celery app available when Django starts
+from .celery import app as celery_app  # noqa
+
+__all__ = ('celery_app',)
